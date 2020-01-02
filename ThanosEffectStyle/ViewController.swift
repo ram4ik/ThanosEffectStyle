bd65fdaeb54db51fb8bb3422fcff0113a497a2bd
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-02 21:38:55 +0200

    Initial Commit

---
+++ ThanosEffectStyle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThanosEffectStyle
+//
+//  Created by Ramill Ibragimov on 02.01.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
